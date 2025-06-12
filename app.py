3e6dd9d49e4f71c5f6c64c58316d6603aed8e190
Krystian <krystian.ostrogorski@gmail.com>
2025-06-12 09:43:10 +0200

    port changes

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+
+port =int(os.environ.get("PORT", 80))
 
 @app.route('/')
 def hello():
     return "Hello from Flask inside Docker!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
